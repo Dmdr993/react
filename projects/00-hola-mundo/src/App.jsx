7b863a9f1568ebc3578a6850884a7753ea7e1d93
Dmdr993 <dmdr993@gmail.com>
2023-07-09 12:33:53 -0500

    Simular Fetch
    Se ha creado un array de objetos para simular un fetch a una API

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -4,50 +4,43 @@ import { useState } from "react";//hooks
 //De hijo a padre no se pueden pasar prop, se hacen por callbacks
 
 export function App() {
-    // https://unavatar.io/#/
-    // Avatar web
-    //Los estilos en linea se mandan como objetos
-
-    //Puedes enviar funciones como parametro
-    // const formatUserName = (userName) => `@${userName}`;
-    const [name, setName] = useState('midudev');
-    const nasa = { initialIsFollowing: false, userName: "nasa", name: "NASA" }
-    //Los componentes crean elementos
-    //React renderiza Elementos
-    //React renderiza de los padres hacia arriba 
-    //Independientemente si hay cambios o no
-    // console.log("Se ha renderizado", name)
+    const users = [
+        {
+            userName: 'midudev',
+            name: 'Miguel Angel',
+            isFollowing: true
+        },
+        {
+            userName: 'pheralb',
+            name: 'Pablo',
+            isFollowing: true
+        },
+        {
+            userName: 'dmdr09',
+            name: 'Diego Duran',
+            isFollowing: true
+        },
+        {
+            userName: 'TMCHeing',
+            name: 'Tomas',
+            isFollowing: false
+        },
+    ]
     return (
         <section className="App">
-            <TwitterFollowCard
-                // Pasando estado como props
-                initialIsFollowing
-                userName={name}
-                name="Midudev"
-            >
-                {/* Lo que envuelve se llama children */}
-                <strong>Midudev</strong>
-            </TwitterFollowCard>
-            <TwitterFollowCard
-
-                initialIsFollowing={true}
-                userName="dmdr09"
-                name="Diego Duran"
-            >
-                <strong>Diego Duran</strong>
-            </TwitterFollowCard>
-            <TwitterFollowCard
-                initialIsFollowing={false}
-                name="Default"
-            >
-                <strong>Valor por defecto</strong>
-            </TwitterFollowCard>
-            <TwitterFollowCard {...nasa}>
-                <strong>Como objeto(Mala práctica)</strong>
-            </TwitterFollowCard>
-            <button onClick={() => setName('pedro')}>
-                Cambio de nombre
-            </button>
-        </section>
+            {
+                users.map(user => {
+                    const { userName, name, isFollowing } = user
+                    return (
+                        <TwitterFollowCard 
+                        userName={userName}
+                        name = { name }
+                        initialIsFollowing = { isFollowing } >
+                            
+                        </TwitterFollowCard>
+    )
+})
+            }
+        </section >
     )
 }
